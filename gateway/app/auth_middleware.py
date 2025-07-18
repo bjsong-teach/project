5da2f788696cc8a50129fe108c85139c281bd3cf
itdaweb <88920549+itdaweb@users.noreply.github.com>
2025-07-18 15:53:47 +0900

    .

--- gateway/app/auth_middleware.py
+++ gateway/app/auth_middleware.py
@@ -10,16 +10,12 @@ USER_SERVICE_URL = os.getenv("USER_SERVICE_URL")
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        print("✅ 요청 처리 시작!")
         public_paths = ["/api/auth/login", "/api/auth/register"]
-        print("--- Modified Headers (request) ---")
-        print(request.headers)
-        print("--------------------------------------")
         if request.method in ("GET","OPTIONS") or request.url.path in public_paths:
             return await call_next(request)
 
         session_id = request.cookies.get("session_id")
-        print(f"Request received: {request.method} {request.url.path}")
+        #print(f"Request received: {request.method} {request.url.path}")
         if not session_id:
             return JSONResponse(status_code=401, content={"detail": "Not authenticated"})
         try:
@@ -43,7 +39,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
 
         response = await call_next(request)
         
-        print("--- Modified Headers (request.headers) ---")
-        print(request.headers)
-        print("--------------------------------------")
+        # print("--- Modified Headers (request.headers) ---")
+        # print(request.headers)
+        # print("--------------------------------------")
         return response
